e1b83681d2ae46a84c2bc9ddbe717b8d2e188fe0
Michael Kuck <me@michael-kuck.com>
2016-06-19 12:12:24 +0800

    Add simple bootstrap script to install brews, casks, setup environment

---
+++ bootstrap.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+function doIt() {
+    # Install homebrew
+    /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+    # Configure OS X
+    sh osx-defaults.sh
+
+    # Install Oh-My-Zsh
+    sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+    # Install brews & casks
+    sh install-brews.sh
+
+    # Install Vundle package manager for VIM
+    git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+
+    cd ~
+    ln -s dotfiles/.aliases
+    ln -s dotfiles/.bash_profile
+    ln -s dotfiles/.ec2cfg
+    ln -s dotfiles/.functions
+    ln -s dotfiles/.gitconfig
+    ln -s dotfiles/.s3cfg
+    ln -s dotfiles/.tmux.conf
+    ln -s dotfiles/.vimrc
+    ln -s dotfiles/.zshrc
+
+    ln -s dotfiles/bin
+
+    mkdir -p .gradle
+    cd .gradle
+    ln -s ~/dotfiles/.gradle/gradle.properties
+    cd ~
+
+    chflags hidden bin
+    chflags hidden dotfiles
+}
+
+if [ "$1" == "--force" -o "$1" == "-f" ]; then
+	doIt;
+else
+	read -p "This may overwrite existing files. Are you sure? (y/n) " -n 1;
+	echo "";
+	if [[ $REPLY =~ ^[Yy]$ ]]; then
+		doIt;
+	fi;
+fi;
+unset doIt;
+
+
+
+
+
---
+++ osx-defaults.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+defaults write com.apple.dock tilesize -int 35
+defaults write com.apple.dock largesize -int 50
+defaults write com.apple.dock magnification -bool true
+
+
