e4bb0ab0d02d009dd07d6fc25ea41c43181e6db1
Michael Kuck <me@michael-kuck.com>
2016-06-21 16:18:10 +0800

    Update brew/cask apps list

--- install-brews.sh
+++ install-brews.sh
@@ -3,15 +3,12 @@
 brew update && brew upgrade
 
 brew install git
-brew install heroku
 brew install htop
-brew install macvim
 brew install netcat
 brew install netperf
 brew install node
 brew install python
 brew install rbenv
-brew install rename
 brew install s3cmd
 brew install sox
 brew install swaks
@@ -21,7 +18,6 @@ brew install tree
 brew install vim
 
 brew cask install aerial
-brew cask install android-file-transfer
 brew cask install appcode
 brew cask install arq
 brew cask install boxer
@@ -31,32 +27,27 @@ brew cask install daisydisk
 brew cask install dash
 brew cask install diffmerge
 brew cask install dropbox
-brew cask install duet
 brew cask install flux
 brew cask install franz
 brew cask install google-chrome
 brew cask install google-hangouts
-brew cask install handbrake
-brew cask install handbrake-batch
 brew cask install icons8
 brew cask install imageoptim
-brew cask install intel-power-gadget
 brew cask install intellij-idea
 brew cask install invisionsync
 brew cask install istat-menus
 brew cask install java
+brew cask install macvim
 brew cask install mou
 brew cask install mp4tools
-brew cask isntall mplayerx
+brew cask install mplayerx
 brew cask install openemu
 brew cask install parallels-desktop
-brew cask install parse
 brew cask install sequel-pro
 brew cask install sketch
 brew cask install skype
 brew cask install sourcetree
 brew cask install sqlitebrowser
-brew cask install teamviewer
 brew cask install transmission
 brew cask install vlc
 brew cask install xbench
