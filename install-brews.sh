2ad1a5517b4472dbd614c6d28f653e1f890222be
Michael Kuck <me@michael-kuck.com>
2016-12-05 14:59:23 +0800

    Update list of installed brews and casks

--- install-brews.sh
+++ install-brews.sh
@@ -7,17 +7,19 @@ brew install htop
 brew install netcat
 brew install netperf
 brew install node
+brew install pidcat
 brew install python
 brew install rbenv
+brew install reattach-to-user-namespace
 brew install s3cmd
 brew install sox
 brew install swaks
+brew install swiftlint
 brew install tmux
 brew install trash
 brew install tree
 brew install vim
 
-brew cask install aerial
 brew cask install appcode
 brew cask install arq
 brew cask install boxer
@@ -26,9 +28,11 @@ brew cask install cyberduck
 brew cask install daisydisk
 brew cask install dash
 brew cask install diffmerge
+brew cask install dolphin
 brew cask install dropbox
 brew cask install flux
 brew cask install franz
+brew cask install geekbench
 brew cask install google-chrome
 brew cask install google-hangouts
 brew cask install icons8
@@ -43,13 +47,16 @@ brew cask install mp4tools
 brew cask install mplayerx
 brew cask install openemu
 brew cask install parallels-desktop
+brew cask install pycharm
 brew cask install sequel-pro
 brew cask install sketch
 brew cask install skype
 brew cask install sourcetree
+brew cask install spotify
 brew cask install sqlitebrowser
 brew cask install transmission
 brew cask install vlc
+brew cask install webstorm
 brew cask install xbench
 brew cask install zeplin
 
