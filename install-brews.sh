d81e96e618e40c8d3423f444f653df6fe775e196
Michael Kuck <me@michael-kuck.com>
2017-07-16 18:38:42 +0800

    Update brews and casks

--- install-brews.sh
+++ install-brews.sh
@@ -3,48 +3,63 @@
 brew update && brew upgrade
 
 brew install antigen
+brew install awscli
+brew install cmake
 brew install ctags
+brew install exiftool
+brew install ffmpeg
 brew install git
-brew install htop
+brew install httpie
+brew install hub
+brew install mplayer
 brew install netcat
 brew install netperf
 brew install node
 brew install pidcat
-brew install python
 brew install rbenv
 brew install reattach-to-user-namespace
 brew install s3cmd
 brew install sox
+brew install speedtest_cli
 brew install swaks
 brew install swiftlint
+brew install tldr
 brew install tmux
 brew install trash
 brew install tree
 brew install vim
+brew install watchman
 
+brew cask install aerial
+brew cask install android-studio
+brew cask install appcleaner
 brew cask install appcode
 brew cask install arq
 brew cask install boxer
 brew cask install caffeine
+brew cask install clion
 brew cask install cyberduck
 brew cask install daisydisk
 brew cask install dash
 brew cask install diffmerge
+brew cask install docker
 brew cask install dolphin
 brew cask install dropbox
-brew cask install flux
-brew cask install franz
+brew cask install fork
 brew cask install geekbench
 brew cask install google-chrome
 brew cask install google-hangouts
+brew cask install handbrake
 brew cask install icons8
 brew cask install imageoptim
 brew cask install intellij-idea
 brew cask install invisionsync
 brew cask install istat-menus
+brew cask install iterm2
 brew cask install java
+brew cask install kitematic
 brew cask install macvim
-brew cask install mou
+brew cask install markright
 brew cask install mp4tools
 brew cask install mplayerx
 brew cask install openemu
@@ -53,13 +68,16 @@ brew cask install pycharm
 brew cask install sequel-pro
 brew cask install sketch
 brew cask install skype
+brew cask install slack
 brew cask install sourcetree
 brew cask install spotify
 brew cask install sqlitebrowser
-brew cask install transmission
+brew cask install steveschow-gfxcardstatus
+brew cask install sublime-text
+brew cask install subtitles
 brew cask install vlc
 brew cask install webstorm
-brew cask install xbench
+brew cask install whatsapp
 brew cask install zeplin
 
 brew doctor
