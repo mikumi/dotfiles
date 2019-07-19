2821464c4aa9b13f7ee5687dacead03229d84e07
Michael Kuck <me@michael-kuck.com>
2019-07-19 15:40:50 +0800

    Update macOS dock configuration

--- configure-dock.sh
+++ configure-dock.sh
@@ -12,26 +12,25 @@ dockutil --add "/Applications/Calendar.app" --no-restart
 dockutil --add "/Applications/Reminders.app" --no-restart	
 dockutil --add "/Applications/Trello.app" --no-restart	
 dockutil --add "/Applications/Notes.app" --no-restart	
-dockutil --add "/Applications/Microsoft OneNote.app" --no-restart	
+dockutil --add "/Applications/Notability.app" --no-restart	
 dockutil --add "/Applications/Day One.app" --no-restart	
 dockutil --add "/Applications/Numbers.app" --no-restart	
 dockutil --add "/Applications/Photos.app" --no-restart	
-dockutil --add "/Applications/Adobe Lightroom Classic CC/Adobe Lightroom Classic CC.app" --no-restart	
+dockutil --add "/Applications/Adobe Lightroom Classic/Adobe Lightroom Classic.app" --no-restart	
 dockutil --add "/Applications/DaVinci Resolve/DaVinci Resolve.app" --no-restart	
 dockutil --add "/Applications/Spotify.app" --no-restart	
 dockutil --add "/Applications/iTunes.app" --no-restart	
-dockutil --add "/Applications/Books.app" --no-restart	
 dockutil --add "/Applications/System Preferences.app" --no-restart	
 dockutil --add "/Applications/Parallels Desktop.app" --no-restart	
 dockutil --add "/Applications/Slack.app" --no-restart	
-dockutil --add "/Applications/Utilities/Terminal.app" --no-restart	
+dockutil --add "/Applications/iTerm.app" --no-restart	
 dockutil --add "/Applications/Utilities/Activity Monitor.app" --no-restart	
-dockutil --add "/Users/michaelkuck/Library/Application Support/JetBrains/Toolbox/apps/PyCharm-P/ch-0/182.4505.26/PyCharm.app" --no-restart	
-dockutil --add "/Users/michaelkuck/Library/Application Support/JetBrains/Toolbox/apps/WebStorm/ch-0/182.5107.19/WebStorm.app" --no-restart	
+dockutil --add "/Applications/PyCharm.app" --no-restart	
+dockutil --add "/Applications/WebStorm.app" --no-restart	
+dockutil --add "/Applications/DataGrip.app" --no-restart	
 dockutil --add "/Applications/Android Studio.app" --no-restart	
-dockutil --add "/Users/michaelkuck/Applications/JetBrains Toolbox/DataGrip.app" --no-restart	
 dockutil --add "/Applications/Xcode.app" --no-restart	
-dockutil --add "/Users/michaelkuck/Library/Application Support/JetBrains/Toolbox/apps/AppCode/ch-0/183.4284.115/AppCode 2018.3 EAP.app" --no-restart	
+dockutil --add "/Applications/AppCode.app" --no-restart	
 dockutil --add "/Applications/Outbank.app" --no-restart	
 dockutil --add "/Users/michaelkuck/Desktop" --view fan --display stack --no-restart
 dockutil --add "/Users/michaelkuck/Documents/Akten" --view fan --display stack --no-restart
