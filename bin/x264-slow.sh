da2c75e7ae654257248c18b6a38fefef9970a820
Michael Kuck <me@michael-kuck.com>
2020-03-01 21:47:48 +0800

    Update movie compression scripts to include better output tags

--- bin/x264-slow.sh
+++ bin/x264-slow.sh
@@ -1,1 +1,1 @@
-cs=$(get-colorspace.sh "$1") && time ffmpeg -i "$1" -c:v libx264 -preset slow -tune film -crf $2 -pix_fmt yuv420p -color_primaries "$cs" -color_trc "$cs" -colorspace "$cs" "$1"_264.mp4
+cs=$(get-colorspace.sh "$1") && time ffmpeg -i "$1" -c:v libx264 -preset slow -tune film -crf $2 -pix_fmt yuv420p -color_primaries "$cs" -color_trc "$cs" -colorspace "$cs" "$1"_x264_crf"$2".mp4
