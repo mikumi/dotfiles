ec486da926a41720af07fcb112f87c507a8c8529
Michael Kuck <me@michael-kuck.com>
2020-03-01 21:48:15 +0800

    Update macOS dock with latest links

--- configure-dock.sh
+++ configure-dock.sh
@@ -1,37 +1,39 @@
-dockutil --add "/Applications/App Store.app" --no-restart	
+dockutil --add "/System/Applications/App Store.app" --no-restart	
 dockutil --add "/Applications/Safari.app" --no-restart	
 dockutil --add "/Applications/Google Chrome.app" --no-restart	
-dockutil --add "/Applications/Mail.app" --no-restart	
-dockutil --add "/Applications/Messages.app" --no-restart	
+dockutil --add "/System/Applications/Mail.app" --no-restart	
+dockutil --add "/System/Applications/Messages.app" --no-restart	
 dockutil --add "/Applications/WhatsApp.app" --no-restart	
 dockutil --add "/Applications/LINE.app" --no-restart	
+dockutil --add "/Applications/Threema.app" --no-restart	
 dockutil --add "/Applications/Messenger.app" --no-restart	
-dockutil --add "/Applications/FaceTime.app" --no-restart	
-dockutil --add "/Applications/Contacts.app" --no-restart	
-dockutil --add "/Applications/Calendar.app" --no-restart	
-dockutil --add "/Applications/Reminders.app" --no-restart	
+dockutil --add "/System/Applications/FaceTime.app" --no-restart	
+dockutil --add "/System/Applications/Contacts.app" --no-restart	
+dockutil --add "/Applications/Fantastical.app" --no-restart	
+dockutil --add "/System/Applications/Reminders.app" --no-restart	
 dockutil --add "/Applications/Trello.app" --no-restart	
-dockutil --add "/Applications/Notes.app" --no-restart	
-dockutil --add "/Applications/Notability.app" --no-restart	
+dockutil --add "/System/Applications/Notes.app" --no-restart	
 dockutil --add "/Applications/Day One.app" --no-restart	
 dockutil --add "/Applications/Numbers.app" --no-restart	
-dockutil --add "/Applications/Photos.app" --no-restart	
+dockutil --add "/System/Applications/Photos.app" --no-restart	
 dockutil --add "/Applications/Adobe Lightroom Classic/Adobe Lightroom Classic.app" --no-restart	
 dockutil --add "/Applications/DaVinci Resolve/DaVinci Resolve.app" --no-restart	
 dockutil --add "/Applications/Spotify.app" --no-restart	
-dockutil --add "/Applications/iTunes.app" --no-restart	
-dockutil --add "/Applications/System Preferences.app" --no-restart	
+dockutil --add "/System/Applications/Music.app" --no-restart	
+dockutil --add "/System/Applications/TV.app" --no-restart	
+dockutil --add "/System/Applications/FindMy.app" --no-restart	
+dockutil --add "/System/Applications/System Preferences.app" --no-restart	
 dockutil --add "/Applications/Parallels Desktop.app" --no-restart	
 dockutil --add "/Applications/Slack.app" --no-restart	
 dockutil --add "/Applications/iTerm.app" --no-restart	
-dockutil --add "/Applications/Utilities/Activity Monitor.app" --no-restart	
-dockutil --add "/Applications/PyCharm.app" --no-restart	
+dockutil --add "/System/Applications/Utilities/Activity Monitor.app" --no-restart	
 dockutil --add "/Applications/WebStorm.app" --no-restart	
-dockutil --add "/Applications/DataGrip.app" --no-restart	
-dockutil --add "/Applications/Android Studio.app" --no-restart	
 dockutil --add "/Applications/Xcode.app" --no-restart	
 dockutil --add "/Applications/AppCode.app" --no-restart	
+dockutil --add "/Applications/Visual Studio Code.app" --no-restart	
+dockutil --add "/Applications/Postman.app" --no-restart	
 dockutil --add "/Applications/Outbank.app" --no-restart	
-dockutil --add "/Users/michaelkuck/Desktop" --view fan --display stack --no-restart
 dockutil --add "/Users/michaelkuck/Documents/Akten" --view fan --display stack --no-restart
+dockutil --add "/Users/michaelkuck/Desktop" --view fan --display stack --no-restart
+dockutil --add "/Users/michaelkuck/Documents" --view fan --display stack --no-restart
 dockutil --add "/Users/michaelkuck/Downloads" --view fan --display stack --no-restart
