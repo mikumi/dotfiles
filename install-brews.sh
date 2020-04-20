3c28eafd6256141a426261e41cbfb2c1c9871aaf
Michael Kuck <me@michael-kuck.com>
2020-04-20 13:25:28 +0800

    Update cask & brew install list

--- install-brews.sh
+++ install-brews.sh
@@ -7,15 +7,16 @@ echo ""
 echo "Installing BASIC brews..."
 echo ""
 
+# BASIC NON-DEV STUFF
 brew install antigen
-brew install ccat
+brew install ccat # color cat
 brew install dockutil
 brew install fzf
 brew install git
 brew install git-lfs
-brew install grc
 brew install jq
 brew install mosh
+brew install nmap
 brew install reattach-to-user-namespace
 brew install speedtest_cli
 brew install telnet
@@ -23,36 +24,48 @@ brew install tmux
 brew install vim
 brew install trash
 
+# DEVELOPMENT AND OTHER DAILY STUFF
 if [ "$INSTALLATION_TYPE" != "BASIC" ]; then
     echo ""
     echo "Installing DEFAULT brews..."
     echo ""
     brew install awscli
     brew install cmake
+    brew install coreutils
     brew install ctags
+    brew install ctop
     brew install docker-compose
     brew install docker-machine
+    brew install doctl
+    brew install fastlane
     brew install htop
     brew install httpie
-    brew install hub
+    brew install nativefier
     brew install netcat
-    brew install netperf
     brew install node
+    brew install nvm
+    brew install pkg-config
     brew install pidcat
     brew install s3cmd
+    brew install speedtest-cli
     brew install sox
     brew install swaks
     brew install swiftlint
+    brew install telnet
     brew install tldr
     brew install tree
+    brew install watch
 fi
 
+# EVERYTHING ELSE
 if [ "$INSTALLATION_TYPE" == "FULL" ]; then
     echo ""
     echo "Installing FULL brews..."
     echo ""
     brew install exiftool
     brew install ffmpeg
+    brew install ghostscript
+    brew install imagemagick
     brew install mpv
     brew install mplayer
 fi
--- install-casks.sh
+++ install-casks.sh
@@ -5,20 +5,25 @@ echo ""
 echo "Installing BASIC casks..."
 echo ""
 
+# BASIC NON-DEV STUFF
 brew cask install 1password
 brew cask install alfred
 brew cask install dropbox
 brew cask install sublime-text
 
+# DEVELOPMENT AND OTHER DAILY STUFF
 if [ "$INSTALLATION_TYPE" != "BASIC" ]; then
     echo ""
     echo "Installing DEFAULT casks..."
     echo ""
+    brew cask install abstract
     brew cask install android-studio
     brew cask install appcleaner
     brew cask install appcode
-    brew cask install backblaze
+    brew cask install backblaze-downloader
     brew cask install bettertouchtool
+    brew cask install cakebrew
+    brew cask install cyberduck
     brew cask install dash
     brew cask install diffmerge
     brew cask install docker
@@ -26,40 +31,51 @@ if [ "$INSTALLATION_TYPE" != "BASIC" ]; then
     brew cask install fastlane
     brew cask install fork
     brew cask install google-chrome
+    brew cask install google-cloud-sdk
     brew cask install icons8
     brew cask install iina
+    brew cask install imageoptim
     brew cask install intel-power-gadget
-    brew cask install istat-menus
+    brew cask install iterm2
     brew cask install jetbrains-toolbox
     brew cask install keepingyouawake
     brew cask install macvim
-    brew cask install nordvpn
+    brew cask install ngrok
     brew cask install sequel-pro
     brew cask install sip
     brew cask install sketch
     brew cask install slack
+    brew cask install turbo-boost-switcher
+    brew cask install visual-studio-code
     brew cask install webstorm
     brew cask install whatsapp
+    brew cask install xquartz
     brew cask install zeplin
 fi
 
+# EVERYTHING ELSE
 if [ "$INSTALLATION_TYPE" == "FULL" ]; then
     echo ""
     echo "Installing FULL casks..."
     echo ""
     brew cask install adobe-creative-cloud
     brew cask install aerial
+    brew cask install arq
+    brew cask install backblaze
+    brew cask install battle-net
     brew cask install boxer
     brew cask install cinebench
     brew cask install coconutbattery
-    brew cask install cyberduck
     brew cask install daisydisk
     brew cask install datagrip
     brew cask install dolphin
+    brew cask install fantastical
+    brew cask install firefox
     brew cask install geekbench
+    brew cask install google-backup-and-sync
     brew cask install handbrake
-    brew cask install imageoptim
     brew cask install intellij-idea
+    brew cask install istat-menus
     brew cask install mactracker
     brew cask install macdown
     brew cask install messenger
@@ -71,7 +87,6 @@ if [ "$INSTALLATION_TYPE" == "FULL" ]; then
     brew cask install skype
     brew cask install spotify
     brew cask install steveschow-gfxcardstatus
-    brew cask install subtitles
     brew cask install upwork
     brew cask install xbench
 fi
