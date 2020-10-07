7801f623e09be145d773a89806500f1c4322b10c
Michael Kuck <me@michael-kuck.com>
2020-10-07 16:45:58 +0800

    Upgrade to latest homebrew install script

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,7 +4,7 @@ function doIt() {
     if [[ `uname` == 'Darwin' ]] ; then
         # Install homebrew
         echo "Installing homebrew..."
-        /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+        /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
         brew tap homebrew/cask-fonts
 
         # Configure OS X
