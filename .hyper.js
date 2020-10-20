d84e2c9afde1cf0d2514746167d075fb3f20e104
Michael Kuck <me@michael-kuck.com>
2020-10-20 16:16:55 +0800

    Update Hyper default colors and font

--- .hyper.js
+++ .hyper.js
@@ -9,11 +9,11 @@ module.exports = {
     updateChannel: 'stable',
 
     // default font size in pixels for all tabs
-    fontSize: 12,
+    fontSize: 13,
 
     // font family with optional fallbacks
     // fontFamily: 'Menlo, "DejaVu Sans Mono", Consolas, "Lucida Console", monospace',
-    fontFamily: '"Fira Code", "Hack Nerd Font"',
+    fontFamily: '"Hack Nerd Font"',
 
     // default font weight: 'normal' or 'bold'
     fontWeight: 'normal',
@@ -22,13 +22,13 @@ module.exports = {
     fontWeightBold: 'bold',
 
     // line height as a relative unit
-    lineHeight: 1.2,
+    lineHeight: 1.1,
 
     // letter spacing as a relative unit
-    letterSpacing: 0,
+    letterSpacing: 1,
 
     // terminal cursor background color and opacity (hex, rgb, hsl, hsv, hwb or cmyk)
-    cursorColor: 'rgba(248,28,229,0.8)',
+    cursorColor: '#CCCCCC',
 
     // terminal text color under BLOCK cursor
     cursorAccentColor: '#000',
@@ -40,14 +40,14 @@ module.exports = {
     cursorBlink: false,
 
     // color of the text
-    foregroundColor: '#fff',
+    foregroundColor: '#CCCCCC',
 
     // terminal background color
     // opacity is only supported on macOS
-    backgroundColor: '#000',
+    backgroundColor: '#1E1E1E',
 
     // terminal selection color
-    selectionColor: 'rgba(248,28,229,0.3)',
+    selectionColor: '#6666',
 
     // border color (window, tabs)
     borderColor: '#333',
@@ -74,22 +74,22 @@ module.exports = {
     // including the 6 x 6 color cubes and the grayscale map, just provide
     // an array here instead of a color map object
     colors: {
-      black: '#000000',
-      red: '#C51E14',
-      green: '#1DC121',
-      yellow: '#C7C329',
-      blue: '#0A2FC4',
-      magenta: '#C839C5',
-      cyan: '#20C5C6',
-      white: '#C7C7C7',
-      lightBlack: '#686868',
-      lightRed: '#FD6F6B',
-      lightGreen: '#67F86F',
-      lightYellow: '#FFFA72',
-      lightBlue: '#6A76FB',
-      lightMagenta: '#FD7CFC',
-      lightCyan: '#68FDFE',
-      lightWhite: '#FFFFFF',
+      black: '#6A787A',
+      red: '#E9653B',
+      green: '#39E9A8',
+      yellow: '#E5B684',
+      blue: '#44AAE6',
+      magenta: '#E17599',
+      cyan: '#3DD5E7',
+      white: '#C3DDE1',
+      lightBlack: '#598489',
+      lightRed: '#E65029',
+      lightGreen: '#00FF9A',
+      lightYellow: '#E89440',
+      lightBlue: '#009AFB',
+      lightMagenta: '#FF578F',
+      lightCyan: '#5FFFFF',
+      lightWhite: '#D9FBFF',
     },
 
     // the shell to run when spawning a new session (i.e. /usr/local/bin/fish)
