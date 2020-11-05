1e3146c1224b23a828b7a4657309e787904e7b44
Michael Kuck <me@michael-kuck.com>
2020-11-05 22:07:11 +0800

    Lazy load nvm, greatly increasing shell startup

---
+++ bin/lazynvm.sh
@@ -0,0 +1,32 @@
+# lazy load nvm
+# all props goes to http://broken-by.me/lazy-load-nvm/
+# grabbed from reddit @ https://www.reddit.com/r/node/comments/4tg5jg/lazy_load_nvm_for_faster_shell_start/
+
+lazynvm() {
+  echo "Lazy loading NVM..."
+  unset -f nvm node npm npx
+  export NVM_DIR="$HOME/.nvm"
+  [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh" # This loads nvm
+  [ -s "/usr/local/opt/nvm/nvm.sh" ] && . "/usr/local/opt/nvm/nvm.sh"  # This loads nvm ]
+  [ -s "/usr/local/opt/nvm/etc/bash_completion" ] && . "/usr/local/opt/nvm/etc/bash_completion" ] # This loads nvm bash_completion
+}
+
+nvm() {
+  lazynvm
+  nvm $@
+}
+
+node() {
+  lazynvm
+  node $@
+}
+
+npm() {
+  lazynvm
+  npm $@
+}
+
+npx() {
+  lazynvm
+  npx $@
+}
