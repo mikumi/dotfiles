658e7fb6d00943be0b7bcf133caf06cdae8311cb
Michael Kuck <me@michael-kuck.com>
2022-02-05 23:09:05 +0800

    Split x265 scripts into medium/fast + 8b/10b

--- bin/x265-medium.sh
+++ bin/x265-medium.sh
@@ -1,1 +1,1 @@
-cs=$(get-colorspace.sh "$1") && time ffmpeg -i "$1" -c:v libx265 -preset medium -tune grain -crf $2 -pix_fmt yuv420p10le -color_primaries "$cs" -color_trc "$cs" -colorspace "$cs" -tag:v hvc1 "$1"_x265_crf"$2".mp4
+cs=$(get-colorspace.sh "$1") && time ffmpeg -i "$1" -c:v libx265 -preset medium -tune grain -crf $2 -pix_fmt yuv420p -color_primaries "$cs" -color_trc "$cs" -colorspace "$cs" -tag:v hvc1 "$1"_x265_crf"$2".mp4
