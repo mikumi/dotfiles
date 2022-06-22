e3f3e56684c5bfd0037377a09357253ee60b181b
Michael Kuck <me@michael-kuck.com>
2022-06-22 09:13:41 +0800

    Use hyper-one-dark theme for hyper

--- .hyper.js
+++ .hyper.js
@@ -148,7 +148,7 @@ module.exports = {
   //   `hyperpower`
   //   `@company/project`
   //   `project#1.0.1`
-  plugins: ["hyper-search", "hyper-font-ligatures"],
+  plugins: ["hyper-search", "hyper-font-ligatures", "hyper-one-dark"],
 
   // in development, you can create a directory under
   // `~/.hyper_plugins/local/` and include it here
