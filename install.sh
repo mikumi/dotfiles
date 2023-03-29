425e760a0d7bcd6cb77967f357223c8526dbf810
Michael Kuck <me@michael-kuck.com>
2023-03-29 13:07:01 +0800

    Add nvm and antigen to linux install

--- install.sh
+++ install.sh
@@ -60,6 +60,10 @@ function installLinux() {
   installCommon
 
   $SUDO chsh -s /bin/zsh $(whoami)
+
+  curl -L git.io/antigen > antigen.zsh
+
+  curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
 }
 
 function installCommon() {
