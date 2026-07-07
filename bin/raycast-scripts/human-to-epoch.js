dee07e7f36c7b94fa40ad6a3e39b22dec4cd2584
Michael Kuck <me@michael-kuck.com>
2026-07-07 12:35:22 +0200

    Add timestamp conversion to Raycast

--- bin/raycast-scripts/human-to-epoch.js
+++ bin/raycast-scripts/human-to-epoch.js
@@ -78,13 +78,13 @@ function _formatISOWithTimezone(date) {
   const sign = offset >= 0 ? '+' : '-';
   const hours = Math.floor(Math.abs(offset) / 60).toString().padStart(2, '0');
   const minutes = (Math.abs(offset) % 60).toString().padStart(2, '0');
-  
+
   const year = date.getFullYear();
   const month = (date.getMonth() + 1).toString().padStart(2, '0');
   const day = date.getDate().toString().padStart(2, '0');
   const hour = date.getHours().toString().padStart(2, '0');
   const minute = date.getMinutes().toString().padStart(2, '0');
   const second = date.getSeconds().toString().padStart(2, '0');
-  
+
   return `${year}-${month}-${day}T${hour}:${minute}:${second}${sign}${hours}${minutes}`;
 }
